5bc19f43c40c54fa0288d30a6bb58aaa6c197779
lalo600 <22123066@bucks.ac.uk>
2024-12-12 05:52:21 +0000

    Update test_library_system.py

--- test_library_system.py
+++ test_library_system.py
@@ -6,3 +6,10 @@ def test_book_catalog():
     assert catalog.find_book(1)["title"] == "1984"
     catalog.delete_book(1)
     assert catalog.find_book(1) == "Book not available"
+
+def test_loan_records():
+    loans = LoanRecords()
+    loans.add_loan(101, 1)
+    assert len(loans.list_user_loans(101)) == 1
+    loans.remove_loan(101, 1)
+    assert len(loans.list_user_loans(101)) == 0
