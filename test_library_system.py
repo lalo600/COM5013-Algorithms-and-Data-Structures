a3ba99a4c4966272056e1ca3f3d35252ad3961d4
lalo600 <22123066@bucks.ac.uk>
2024-12-12 06:12:47 +0000

    Update test_library_system.py

--- test_library_system.py
+++ test_library_system.py
@@ -2,4 +2,8 @@ from library_system import LibraryCatalog, BorrowingRecords
 
 def test_library_catalog():
    catalog = LibraryCatalog()
+   catalog.add_book(1, {"title": "1984", "author": "George Orwell"})
+    assert catalog.search_book(1)["title"] == "1984"
+    catalog.remove_book(1)
+    assert catalog.search_book(1) == "Book not found"
 
