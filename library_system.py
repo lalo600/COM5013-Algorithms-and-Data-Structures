8b06fee2f1e7514f6e808670692be4eb9e875867
lalo600 <22123066@bucks.ac.uk>
2024-12-12 05:33:32 +0000

    Update library_system.py

--- library_system.py
+++ library_system.py
@@ -10,3 +10,17 @@ class BookCatalog:
 
     def find_book(self, book_id):
         return self.books.get(book_id, "Book not available")
+
+
+class LoanRecords:
+    def __init__(self):
+        self.loans = []
+
+    def add_loan(self, user_id, book_id):
+        self.loans.append({"user_id": user_id, "book_id": book_id})
+
+    def remove_loan(self, user_id, book_id):
+        self.loans = [loan for loan in self.loans if not (loan["user_id"] == user_id and loan["book_id"] == book_id)]
+
+    def list_user_loans(self, user_id):
+        return [loan for loan in self.loans if loan["user_id"] == user_id]
