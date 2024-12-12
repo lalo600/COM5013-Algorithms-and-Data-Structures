88c879a1d7d666f0b8c26b398ab04d43e84ea129
lalo600 <22123066@bucks.ac.uk>
2024-12-12 05:31:53 +0000

    Update library_system.py

--- library_system.py
+++ library_system.py
@@ -1,3 +1,12 @@
 class BookCatalog:
   def _init_(self):
     self.books = {}
+
+def add_book(self, book_id, book_info):
+        self.catalog[book_id] = book_info
+
+    def remove_book(self, book_id):
+        self.catalog.pop(book_id, None)
+
+    def search_book(self, book_id):
+        return self.catalog.get(book_id, "Book not found")
